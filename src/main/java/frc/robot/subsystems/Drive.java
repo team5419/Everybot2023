6d8b929da7a20637ced4d9892d0ceed86ce42bd5
teo67 <teogerst@gmail.com>
2023-10-26 18:52:44 -0700

    added right motor code

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -11,15 +11,18 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Drive extends SubsystemBase {
   // Define member variables
-  // TODO: SET CAN ID(s)
-  private final int leftLeadID = 0;
-  private final int leftFollowID = 0;
+  // SET CAN ID(s)
+  private final int leftLeadID = 12;
+  private final int leftFollowID = 13;
+  private final int rightLeadID = 22;
+  private final int rightFollowID = 23;
 
   private final CANSparkMax leftLeadMotor;
   private final CANSparkMax leftFollowMotor;
 
-  // TODO: add the right follower for the same for the right side
+  // add the right follower for the same for the right side
   private final CANSparkMax rightLeadMotor;
+  private final CANSparkMax rightFollowMotor;
 
   DifferentialDrive drivetrain;
 
@@ -36,11 +39,14 @@ public class Drive extends SubsystemBase {
     // Initialize or "create" the 2 motor controllers + motors
     leftLeadMotor = new CANSparkMax(leftLeadID, MotorType.kBrushless);
     leftFollowMotor = new CANSparkMax(leftFollowID, MotorType.kBrushless);
+    rightLeadMotor = new CANSparkMax(rightLeadID, MotorType.kBrushless);
+    rightFollowMotor = new CANSparkMax(rightFollowID, MotorType.kBrushless);
 
     // link the control of the two motors together, since they drive one common gearbox
     leftFollowMotor.follow(leftLeadMotor);
 
     // TODO: Same for right side
+    rightFollowMotor.follow(rightLeadMotor);
 
     // class that contains all the wpilib control methods
     drivetrain = new DifferentialDrive(leftLeadMotor, rightLeadMotor);
