af1283af5aeea051a4386ae39c53dc0f11dbf405
[ari esterson] <[aaronestersonn@students.berkeley.net>
2023-11-04 12:24:19 -0700

    started arm

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,30 +1,43 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems;
-
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
 
-  // TODO: ADD AND SET CAN ID(s)
-
-  // TODO: DECLARE MOTOR (CANSparkMax object)
-
+  // defines can ID
+  private final int armID = 0; //change this later
+  //defines motor
+  private final CANSparkMax ArmMotor;
+  //defines encoder
+  private RelativeEncoder m_encoder;
   // set motor current limits
   private final int ARM_CURRENT_LIMIT = 20;
+  // set arm targets for low, medium, high, and platform intake.
+  private final int armTarget;
 
-  // TODO: DEFINE ARM POSITIONS FOR LOW, MEDIUM, HIGH, AND PLATFORM INTAKE
+  private final int armLow;
+  private final int armHigh;
+  private final int armMedium;
+
+
 
   // TODO: DECLARE SHUFFLEBOARD ENTRIES FOR ARM MOTOR TICKS AND ARM PID
   // public static final
 
   public Arm() {
-    // TODO: Initialize motor controller
-
-    // TODO: set current limit
-    // m_Arm.setSmartCurrentLimit(ARM_CURRENT_LIMIT);
+    ArmMotor = new CANSparkMax(armID,MotorType.kBrushless);
+    ArmMotor.setSmartCurrentLimit(ARM_CURRENT_LIMIT);
+    armTarget = 1;
+    armLow = 1;
+    armHigh = 1;
+    armMedium = 1;
 
     // TODO: set motor in brake mode so that the motor holds position even when not given a command
     // m_arm.setIdleMode(IdleMode.kBrake);
@@ -55,13 +68,17 @@ public class Arm extends SubsystemBase {
   // TODO: ADD MOTOR ACCESSORS FOR SETTING TARGET POSITION
   // Other classes/commands do not have access to the private motor object so you have to make it
   // accessible
-  public void setArmTarget() {}
+  public void setArmTarget(int target) {
+    //armTarget = target;
+  }
 
   public void getArmPosition() {}
   // TODO: ADD MOTOR ACCESSORS FOR ZEROING MOTOR POWER AND MOTOR ENCODER
   public void stopArmInput() {}
 
-  public void zeroArmEncoder() {}
+  public void zeroArmEncoder() {
+    //armEncoder.setPosition(0.0);
+  }
 
   // TODO: DETECT IF ARM HAS HIT A HARDSTOP (check motor current)
   public boolean hasHitHardstop() {
