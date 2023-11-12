314d92a6af71fc0f6d431f4cab8e67b2ee30ddbc
rift <rift685@gmail.com>
2023-11-11 16:57:34 -0800

    changed auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -101,7 +101,7 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     // return Autos.exampleAuto(m_exampleSubsystem);
-    return new AutoJitter(drive);
+    return new AutoMobility(drive);
   }
 
   public void setDriveToBrake() {
